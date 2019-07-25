ff1feb51366e56cf51266e83284c0cd6c355e2cf
vincent <chenzhun.huang@mail.mcgill.ca>
2019-07-25 18:01:51 -0400

    Added templates and linked the templates

--- effusion_a_cappella/urls.py
+++ effusion_a_cappella/urls.py
@@ -17,7 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import events.views
+import members.views
+import videos.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', events.views.home, name='home'),
+    path('events', events.views.events, name='events'),
+    path('members', members.views.members, name='members'),
+    path('videos', videos.views.videos, name='videos')
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
