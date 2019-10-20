ca65a6d0b05200a02d388b7dabe082473cd8de2d
Vincent Huang <chenzhun.huang@mail.mcgill.ca>
2019-10-19 23:10:45 -0400

    adjusted email sending function

--- emailsender/views.py
+++ emailsender/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+
 
 def emailsender(request):
+    send_mail(
+        'Hello',
+        'Hello this is the body',
+        'chenzhun.huang@gmail.com',
+        ['chenzhun.huang@mail.mcgill.ca'],
+        fail_silently=False
+    )
     return render(request, 'emailsender/emailsender.html')
